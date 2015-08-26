e2b1044dfe60e7a469a7ccbd93124dda08cdcc87
谢俊伟 <getfilebox@gmail.com>
2015-08-26 14:58:34 +0800

    init

---
+++ HXQandAView/HXQandAView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXQandAView
+//
+//  Created by MacBook on 15/8/26.
+//  Copyright (c) 2015年 MacBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
